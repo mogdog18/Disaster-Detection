ab3639f9788e4e21e9d4e66f57d724b992e582c4
ankile <lars.ankile@gmail.com>
2023-05-06 12:39:56 -0400

    Add helperfunction

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,7 @@ from .disaster_sam import DisasterSamV1, DisasterSamV2
 class ModelRegistry:
     DisasterSamV1 = DisasterSamV1
     DisasterSamV2 = DisasterSamV2
+
+    @classmethod
+    def get_model(cls, name):
+        return getattr(cls, name)
